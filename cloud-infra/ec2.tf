a45e459f035de788f4d2681762b2527a082b21f6
nayra000 <youssefnayra26@gmail.com>
2025-05-18 12:30:01 +0300

    configure jenkins slave

--- cloud-infra/ec2.tf
+++ cloud-infra/ec2.tf
@@ -1,24 +1,31 @@
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "my-key"
+  public_key = file("/home/nayra/project/id_rsa.pub") 
+}
+
+
 
 resource "aws_instance" "bastion" {
-  ami           = "ami-0e449927258d45bc4" 
+  ami           = "ami-084568db4383264d4" 
   instance_type = "t2.micro"             
   subnet_id     = aws_subnet.public_subnet1.id
   vpc_security_group_ids = [
     aws_security_group.allow_ssh_and_https_anywhere.id
   ]
+  key_name = aws_key_pair.ssh_key. key_name
   tags = {
     Name = "Bastion-Host"
   }
 }
 
 resource "aws_instance" "application" {
-  ami           = "ami-0e449927258d45bc4" 
+  ami           = "ami-084568db4383264d4" 
   instance_type = "t2.micro"             
   subnet_id     = aws_subnet.private_subnet1.id
   vpc_security_group_ids = [
     aws_security_group.allow_ssh_and_3000_vpc.id
   ]
-
+  key_name = aws_key_pair.ssh_key. key_name 
   tags = {
     Name = "Application-Host"
   }
