1eaaf45a2ad75d32bd3dd2b85f3b5d104f305cc4
nayra000 <youssefnayra26@gmail.com>
2025-05-20 17:45:10 +0300

    modify version of docker image to latest

--- nodejs_project/tests/employees.test.js
+++ nodejs_project/tests/employees.test.js
@@ -14,10 +14,7 @@ beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
   const uri = mongoServer.getUri();
 
-  await mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
+  await mongoose.connect(uri);
 });
 
 afterAll(async () => {
